8047559646905d35d23af12338d7dc66cee9bd3f
dorian-aq <dorian.epitech@gmail.com>
2020-01-15 11:14:23 +0100

    More context

--- tp-1/exo4.ts
+++ tp-1/exo4.ts
@@ -1,6 +1,8 @@
 const array: number[] = [1, 2, 3, 4];
 
-function displayArray() {
+function displayArray(arr) {
+    // Pas le droit à la boucle for/while
+    // Utilisez la méthode map ou forEach d'un object de type array
 }
 
 displayArray(array);
